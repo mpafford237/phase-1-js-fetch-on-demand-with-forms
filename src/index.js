728c000c4ef1ee1614e474ce2d82d15b0cdc2adf
masonPafford <mdpafford2015@gmail.com>
2024-04-05 12:55:23 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+//1. Prevents page from refreshing whenever submit event happens
+//2. 
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    // could also use event.target.children[1].value, input is valid here because it is the first input found by query selector with that ID. could also querySelectorAll values into an array.
+    const input = inputForm.querySelector("input#searchByID")
+    console.log(input.value)
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then ((data) => {
+        console.log(data)
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p")
+        title.innerText = data.title
+        summary.innerText = data.summary
+    })
+  })
 }
 
+//runs init function once DOM content has loaded
 document.addEventListener('DOMContentLoaded', init);
